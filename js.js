2b7f6eea488ef885fffcc2b7b799baa92829d1f5
Asem <ismailovaasem363@gmail.com>
2025-01-23 14:22:01 +0500

    HM4 part2

---
+++ js.js
@@ -0,0 +1,12 @@
+function ageUser (age) {
+  if (age < 18) {
+    console.log('Wait for ' + (18 - age) + ' years.');
+  } else if (age <= 21) {
+    console.log('You can work, pay taxes, drive and so on.'); 
+  } else {
+    console.log('You can do anything within the law.')
+  }
+}
+ageUser(14)
+ageUser(22)
+ageUser(19)
